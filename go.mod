992d810b066eb50ebf76689de04ba367a43ce6c6
leoantony72 <leoantony102@gmail.com>
2025-03-04 21:03:43 +0530

    feat: grpc functions

--- go.mod
+++ go.mod
@@ -3,12 +3,18 @@ module github.com/leoantony72/disql
 go 1.22.4
 
 require (
-	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	google.golang.org/grpc v1.70.0
+	google.golang.org/protobuf v1.36.5
+	gorm.io/driver/sqlite v1.5.7
+	gorm.io/gorm v1.25.12
+)
+
+require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	gorm.io/driver/mysql v1.5.7 // indirect
-	gorm.io/driver/sqlite v1.5.7 // indirect
-	gorm.io/gorm v1.25.12 // indirect
+	golang.org/x/net v0.32.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241202173237-19429a94021a // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,55 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"os"
 	"strings"
+	"sync"
 
 	"github.com/leoantony72/disql/db"
+	pb "github.com/leoantony72/disql/disql"
+	"google.golang.org/grpc"
 	"gorm.io/gorm"
 )
+
+type server struct {
+	pb.UnimplementedParticipantServer
+	mu         sync.Mutex
+	connection []string
+	data       map[string]string
+	db         *gorm.DB
+}
+
+func (s *server) NewConnection(ctx context.Context, req *pb.NewConnectionRequest) (*pb.NewConnectionResponse, error) {
+	s.connection = append(s.connection, req.Url)
+	return &pb.NewConnectionResponse{Success: true}, nil
+}
+func (s *server) Prepare(ctx context.Context, req *pb.PrepareRequest) (*pb.PrepareResponse, error) {
+	if _, ok := s.data[req.CmdId]; !ok {
+		s.data[req.CmdId] = req.Cmd
+		return &pb.PrepareResponse{Success: true}, nil
+	}
+	return &pb.PrepareResponse{Success: false}, nil
+}
+
+func (s *server) Commit(ctx context.Context, req *pb.CommitRequest) (*pb.CommitResponse, error) {
+	if val, ok := s.data[req.CmdId]; ok {
+		succes := s.ExecuteSQLGrpc(val)
+		if !succes {
+			return &pb.CommitResponse{Success: false}, nil
+		}
+		delete(s.data, req.CmdId)
+		return &pb.CommitResponse{Success: true}, nil
+	}
+	return &pb.CommitResponse{Success: false}, nil
+}
+
+func (s *server) RollBack(ctx context.Context, req *pb.RollBackRequest) (*pb.RollBackResponse, error) {
+	delete(s.data, req.CmdId)
+	return &pb.RollBackResponse{Success: true}, nil
+}
 
 func main() {
 	lis, err := net.Listen("tcp", ":8000")
@@ -19,6 +60,10 @@ func main() {
 	}
 	fmt.Println("tcp server listening on port :8000")
 	db, _ := db.StartDb()
+	grpcServer := grpc.NewServer()
+	server := &server{}
+	pb.RegisterParticipantServer(grpcServer, server)
+
 	for {
 		conn, err := lis.Accept()
 		if err != nil {
@@ -28,11 +73,11 @@ func main() {
 		}
 
 		conn.Write([]byte("connected to tcp server :8000\n"))
-		ReceiveMessages(conn, db)
+		ReceiveMessages(conn, db, server)
 	}
 }
 
-func ReceiveMessages(c net.Conn, db *gorm.DB) {
+func ReceiveMessages(c net.Conn, db *gorm.DB, s *server) {
 	buffer := make([]byte, 100)
 	defer c.Close()
 	for {
@@ -43,8 +88,15 @@ func ReceiveMessages(c net.Conn, db *gorm.DB) {
 			continue
 		}
 		cmd := string(buffer[0:n])
+
 		ExecuteSQL(c, db, cmd)
 
+	}
+}
+
+func Coordinate(s *server) {
+	for _,v := range s.connection{
+		
 	}
 }
 
@@ -94,6 +146,46 @@ func ExecuteSQL(c net.Conn, db *gorm.DB, cmd string) {
 			c.Write([]byte("Query executed successfully.\n"))
 		}
 	}
+}
+
+func (s *server) ExecuteSQLGrpc(cmd string) bool {
+	if isQueryReturningResults(cmd) {
+		rows, err := s.db.Raw(cmd).Rows()
+		if err != nil {
+			return false
+		}
+		defer rows.Close()
+
+		var results []string
+		cols, _ := rows.Columns()
+
+		// Iterate over rows
+		for rows.Next() {
+			values := make([]interface{}, len(cols))
+			valuePtrs := make([]interface{}, len(cols))
+			for i := range values {
+				valuePtrs[i] = &values[i]
+			}
+
+			if err := rows.Scan(valuePtrs...); err != nil {
+				continue
+			}
+
+			rowStr := ""
+			for i, col := range cols {
+				rowStr += fmt.Sprintf("%s: %v | ", col, values[i])
+			}
+			results = append(results, rowStr)
+		}
+	} else {
+		// Execute command without returning results
+		if err := s.db.Exec(cmd).Error; err != nil {
+			return false
+		} else {
+			return true
+		}
+	}
+	return false
 }
 
 func isQueryReturningResults(query string) bool {
